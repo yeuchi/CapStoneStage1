0c8a93d90c53045b489fdf00f6dd712f38d45ca1
yeuchi@gmail.com <yeuchi@gmail.com>
2018-07-29 09:27:19 -0500

    Tab pages
    1. add 4 tab pages
    2. add share button
    3. no menu, no tab menu

--- app/src/main/java/com/example/ctyeung/capstonestage1/MainActivity.java
+++ app/src/main/java/com/example/ctyeung/capstonestage1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ctyeung.capstonestage1;
 
 import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -10,6 +12,7 @@ import com.example.ctyeung.capstonestage1.ViewerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FloatingActionButton btnShare;
     private Button btnViewer;
     private Context context;
 
@@ -19,12 +22,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         context = this;
 
+        // Launch Viewer -- consume
         btnViewer = (Button)findViewById(R.id.btnViewer);
 
         btnViewer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(context, ViewerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Launch Tab pages -- Author + share
+        btnShare = (FloatingActionButton)findViewById(R.id.share_fab);
+
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(getActivity())
+                Intent intent = new Intent(context, TabActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/ctyeung/capstonestage1/TabActivity.java
+++ app/src/main/java/com/example/ctyeung/capstonestage1/TabActivity.java
@@ -5,11 +5,14 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,132 +21,66 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
-public class TabActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+import com.example.ctyeung.capstonestage1.TabFragment1;
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v13.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
+/*
+ * Reference:
+ *
+ * Tab pages
+ * https://www.truiton.com/2015/06/android-tabs-example-fragments-viewpager/
+ * http://www.gadgetsaint.com/android/create-viewpager-tabs-android/#.W13LpC2ZPOQ
+ */
+public class TabActivity extends AppCompatActivity {
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
 
+        // Add Fragments to adapter one by one
+        adapter.addFragment(new TabFragment1(), "Address");
+        adapter.addFragment(new TabFragment2(), "Text");
+        adapter.addFragment(new TabFragment3(), "Shape");
+        adapter.addFragment(new TabFragment4(), "Preview");
+        viewPager.setAdapter(adapter);
 
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
+    // Adapter for the viewpager using FragmentPagerAdapter
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tab, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_tab, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
         }
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            return mFragmentList.get(position);
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 4;
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Address";
-                case 1:
-                    return "Text";
-                case 2:
-                    return "Shape";
-                case 3:
-                    return "Preview";
-            }
-            return null;
+            return mFragmentTitleList.get(position);
         }
     }
 }
