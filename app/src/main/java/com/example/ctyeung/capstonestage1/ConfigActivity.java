8a78138d7d786bf8bed63f5cbfbcebe6607cc6f7
yeuchi@gmail.com <yeuchi@gmail.com>
2018-07-28 19:01:01 -0500

    start activities
    viewer, config

---
+++ app/src/main/java/com/example/ctyeung/capstonestage1/ConfigActivity.java
@@ -0,0 +1,14 @@
+package com.example.ctyeung.capstonestage1;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ConfigActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+
+}
