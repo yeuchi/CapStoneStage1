a084e8ae06ee88966a2cd47965273f682d2c1695
yeuchi@gmail.com <yeuchi@gmail.com>
2018-07-28 21:06:10 -0500

    click button to viewer

--- app/src/main/java/com/example/ctyeung/capstonestage1/MainActivity.java
+++ app/src/main/java/com/example/ctyeung/capstonestage1/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.ctyeung.capstonestage1;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+import com.example.ctyeung.capstonestage1.ViewerActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnViewer;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+
+        btnViewer = (Button)findViewById(R.id.btnViewer);
+
+        btnViewer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, ViewerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
