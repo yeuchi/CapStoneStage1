ac6c5504bcfd26c1a37d41d3ea6ee58bd83cd085
yeuchi@gmail.com <yeuchi@gmail.com>
2018-08-05 19:26:58 -0500

    Preview tab buttons
    1. send
    2. cancel

--- app/src/main/java/com/example/ctyeung/capstonestage1/TabFragment4.java
+++ app/src/main/java/com/example/ctyeung/capstonestage1/TabFragment4.java
@@ -5,11 +5,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class TabFragment4 extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_4, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View rootView = inflater.inflate(R.layout.tab_fragment_4, container, false);
+
+        // Render Preview here ...
+
+
+        Button btnSend = (Button)rootView.findViewById(R.id.btnSend);
+        btnSend.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Send message
+            }
+        });
+
+        Button btnCancel = (Button)rootView.findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Back to MainActivity
+            }
+        });
+
+        return rootView;
     }
 }
