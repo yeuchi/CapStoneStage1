90d4a8a1303607cfa1ea403719ea539298444a45
yeuchi@gmail.com <yeuchi@gmail.com>
2018-08-08 19:33:47 -0500

    cancel button
    return to main page.

--- app/src/main/java/com/example/ctyeung/capstonestage1/TabFragment4.java
+++ app/src/main/java/com/example/ctyeung/capstonestage1/TabFragment4.java
@@ -1,18 +1,24 @@
 package com.example.ctyeung.capstonestage1;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 public class TabFragment4 extends Fragment {
 
+    private Context mContext;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View rootView = inflater.inflate(R.layout.tab_fragment_4, container, false);
+        mContext = rootView.getContext();
 
         // Render Preview here ...
 
@@ -34,6 +40,9 @@ public class TabFragment4 extends Fragment {
             public void onClick(View v)
             {
                 // Back to MainActivity
+                Intent homeIntent = new Intent(mContext, MainActivity.class);
+                homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(homeIntent);
             }
         });
 
