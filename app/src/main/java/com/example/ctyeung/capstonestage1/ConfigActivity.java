dd9145ae5c3ff938f0fefbe704b1ff1625061cb8
yeuchi@gmail.com <yeuchi@gmail.com>
2018-08-11 19:56:06 -0500

    clean up toolbar
    1. config-activity: home arrow.
    2. removed unused toolbar section.

--- app/src/main/java/com/example/ctyeung/capstonestage1/ConfigActivity.java
+++ app/src/main/java/com/example/ctyeung/capstonestage1/ConfigActivity.java
@@ -1,14 +1,33 @@
 package com.example.ctyeung.capstonestage1;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-public class ConfigActivity extends Activity {
+public class ConfigActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case android.R.id.home:
+                Intent homeIntent = new Intent(this, MainActivity.class);
+                homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(homeIntent);
+        }
+        return (super.onOptionsItemSelected(menuItem));
     }
 
 }
