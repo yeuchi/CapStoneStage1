beba8526eca9c4d9b37d54a828331c3b490980d4
yeuchi@gmail.com <yeuchi@gmail.com>
2018-08-19 20:59:49 -0500

    load SVG
    1. playing with AndroidSVG.
    2. document edit - SVG instead of PNG.

--- app/src/main/java/com/example/ctyeung/capstonestage1/TabFragment3.java
+++ app/src/main/java/com/example/ctyeung/capstonestage1/TabFragment3.java
@@ -1,15 +1,53 @@
 package com.example.ctyeung.capstonestage1;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.graphics.drawable.PictureDrawable;
+import android.widget.LinearLayout;
+
+import com.caverock.androidsvg.SVG;
+import com.caverock.androidsvg.SVGImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class TabFragment3 extends Fragment {
 
+    private SVGImageView imageView;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_3, container, false);
+        View root = inflater.inflate(R.layout.tab_fragment_3, container, false);
+
+        Context context = root.getContext();
+        String[] list;
+        try
+        {
+            // replace with networkUtil to load assets ?
+
+            list = context.getAssets().list("");
+            GridLayout layout = root.findViewById(R.id.grid_shapes);
+
+            for(String s : list)
+            {
+                SVGImageView svgImageView = new SVGImageView(context);
+                svgImageView.setImageAsset(s);
+                layout.addView(svgImageView, 0);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return root;
     }
 }
