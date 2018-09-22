b0fb56805e217b9fbbd9f8b9cf0438ff388db97c
yeuchi@gmail.com <yeuchi@gmail.com>
2018-09-21 21:04:59 -0500

    GMail sender
    Code from Apache + Stack Overflow

--- app/src/main/java/com/example/ctyeung/capstonestage1/TabFragment1.java
+++ app/src/main/java/com/example/ctyeung/capstonestage1/TabFragment1.java
@@ -1,15 +1,13 @@
 package com.example.ctyeung.capstonestage1;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
 
-import com.example.ctyeung.capstonestage1.data.SharedPrefUtility;
-import com.example.ctyeung.capstonestage1.utilities.BitmapRenderer;
+import com.example.ctyeung.capstonestage1.gmail.GMailSender;
 
 /*
  * Set method of persistence (gmail, facebook, google-drive, etc)
@@ -39,6 +37,19 @@ public class TabFragment1 extends Fragment {
         else
         {
 
+        }
+    }
+
+    protected void onClickEmail()
+    {
+        try {
+            GMailSender sender = new GMailSender("username@gmail.com", "password");
+            sender.sendMail("This is Subject",
+                    "This is Body",
+                    "user@gmail.com",
+                    "user@yahoo.com");
+        } catch (Exception e) {
+            Log.e("SendMail", e.getMessage(), e);
         }
     }
 }
