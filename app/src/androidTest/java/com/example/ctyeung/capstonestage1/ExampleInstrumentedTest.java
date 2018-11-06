50078bb5cae5499755ad09aaf8079e28a6d487df
yeuchi@gmail.com <yeuchi@gmail.com>
2018-11-06 16:50:05 -0600

    arabic + test
    1. work-in-progress espresso test.
    2. arabic localization for icons.

--- app/src/androidTest/java/com/example/ctyeung/capstonestage1/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/ctyeung/capstonestage1/ExampleInstrumentedTest.java
@@ -4,9 +4,24 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.example.ctyeung.capstonestage1.data.ShapeHelper;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -16,6 +31,19 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<TabActivity> mActivityRule = new ActivityTestRule<>(
+            TabActivity.class);
+
+    @Before
+    public void init(){
+        mActivityRule.getActivity()
+                .getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.tabContainer, new TabFragment3(), "fragment-tag").commitAllowingStateLoss();
+    }
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -23,4 +51,31 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.ctyeung.capstonestage1", appContext.getPackageName());
     }
+
+    @Test
+    public void AllGridListItemsDisplayed()
+    {
+        //onData(withText(R.string.in_email)).perform(click());
+
+        int count = (ShapeHelper.isEnglish())?38:63;
+        //onView(withId (R.id.rv_shapes)).check(matches(withListSize(count)));
+    }
+
+    private static Matcher<View> withListSize(final int count) {
+        return new TypeSafeMatcher<View>() {
+            @Override
+            protected boolean matchesSafely(View view)
+            {
+                RecyclerView recyclerView = view.findViewById(R.id.rv_shapes);
+                int numChildren = recyclerView.getChildCount();
+                return (count==numChildren)?true:false;
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("");
+            }
+        };
+    }
+
 }
